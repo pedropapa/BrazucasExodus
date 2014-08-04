a8d47e94e3af1c539a6a8830e644b293b4585b2b
Pedro Papadópolis <pplp93@gmail.com>
2014-08-03 21:38:37 -0300

    [ADD] - Model Usuario criada para armazenar os clients que acessam o UCP, a princípio apenas para testes.
    [TODO]
    - Verificar como restringir o acesso à manipulação das models, atualmente qualquer requisição direta faz qualquer tipo de instrução, exemplo: /Usuario/create?username=teste&password=testando

---
+++ api/models/Usuario.js
@@ -0,0 +1,39 @@
+/**
+ * Usuario
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+    beforeCreate: function (attrs, next) {
+        var bcrypt = require('bcryptjs');
+
+        bcrypt.genSalt(10, function(err, salt) {
+            if (err) return next(err);
+
+            bcrypt.hash(attrs.password, salt, function(err, hash) {
+                if (err) return next(err);
+
+                attrs.password = hash;
+                next();
+            });
+        });
+    },
+
+    attributes: {
+        username: {
+            type: 'string',
+            unique: true,
+            required: true
+        },
+        password: {
+            type: 'string',
+            required: true,
+            minLength: 6
+        }
+    }
+
+};
