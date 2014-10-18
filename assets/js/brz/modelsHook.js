ab79232d7ef8c9c08add7e610ea3ff1d7a663aa1
Pedro Papadópolis <pedro.papadopolis@apsstecnologia.com.br>
2014-10-18 13:05:00 -0300

    Correção de charset

--- assets/js/brz/modelsHook.js
+++ assets/js/brz/modelsHook.js
@@ -1,10 +1,10 @@
 var socketDisconnected = false;
 
 $(document).ready(function() {
-  // Respons·vel por receber qualquer tipo de dado enviado pela aplicaÁ„o via socket.
+  // Respons√°vel por receber qualquer tipo de dado enviado pela aplica√ß√£o via socket.
   socket.on('message', function(data) {
     console.log(data);
-    // Onde o modelsHook È iniciado.
+    // Onde o modelsHook √© iniciado.
     if(data.model) {
       modelsHook[data.model][data.verb]((data.data)?data.data:data.previous);
     }
@@ -26,29 +26,29 @@ $(document).ready(function() {
   socket.on('disconnect', function(data) {
     socketDisconnected = true;
     // @TODO
-    // Desativar o webchat, competitivo e qualquer outro mÛdulo do ucp que tenha conectividade via socket.
-    // Obrigar o usu·rio a atualizar a p·gina ao reestabelecer a conex„o.
-    noty({text: 'A conex„o com o servidor foi perdida, tentando reconnectar...', type: 'error', killer: true, timeout: false, closeWith: []});
+    // Desativar o webchat, competitivo e qualquer outro m√≥dulo do ucp que tenha conectividade via socket.
+    // Obrigar o usu√°rio a atualizar a p√°gina ao reestabelecer a conex√£o.
+    noty({text: 'A conex√£o com o servidor foi perdida, tentando reconnectar...', type: 'error', killer: true, timeout: false, closeWith: []});
   });
 
   socket.on('connect', function(data) {
     // @TODO
-    // Se for um reconnect, obrigar o usu·rio a atualizar a p·gina.
+    // Se for um reconnect, obrigar o usu√°rio a atualizar a p√°gina.
     if(socketDisconnected) {
       $.noty.closeAll();
-      noty({text: 'Conex„o reestabelecida!', type: 'success', killer: true, timeout: 3000});
+      noty({text: 'Conex√£o reestabelecida!', type: 'success', killer: true, timeout: 3000});
     }
   });
 });
 
 /**
  * modelsHook
- * Respons·vel por receber qualquer tipo de instruÁ„o realizada em uma determinada model.
- * Sendo a instruÁ„o de cada model criada da seguinte maneira: 'model.instruÁ„o()'.
+ * Respons√°vel por receber qualquer tipo de instru√ß√£o realizada em uma determinada model.
+ * Sendo a instru√ß√£o de cada model criada da seguinte maneira: 'model.instru√ß√£o()'.
  */
 var modelsHook = {
   // Usuario.js Model
-  // Respons·vel por gerenciar os usu·rios da aplicaÁ„o.
+  // Respons√°vel por gerenciar os usu√°rios da aplica√ß√£o.
   usuario: {
     update: function(data) {
       alert(data.toSource());
